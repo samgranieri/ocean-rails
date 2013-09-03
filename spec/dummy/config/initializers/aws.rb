54330f79d11eb97568ee1efd2889412a435f6bf1
Peter Bengtson <peter@peterbengtson.com>
2013-09-03 18:36:42 +0200

    Removed spec/dummy/config/config.yml from source control.

--- spec/dummy/config/initializers/aws.rb
+++ spec/dummy/config/initializers/aws.rb
@@ -1,8 +1,11 @@
-# AWS.config access_key_id:     AWS_ACCESS_KEY_ID, 
-#            secret_access_key: AWS_SECRET_ACCESS_KEY, 
-#            region:            AWS_REGION,
-#            user_agent_prefix: "",
-#            dynamo_db:         { :api_version => '2012-08-10' }
+require "aws-sdk"
 
-# $ddb = AWS::DynamoDB.new
+
+AWS.config access_key_id:     AWS_ACCESS_KEY_ID, 
+           secret_access_key: AWS_SECRET_ACCESS_KEY, 
+           region:            AWS_REGION,
+           user_agent_prefix: "",
+           dynamo_db:         { :api_version => '2012-08-10' }
+
+$ddb = AWS::DynamoDB.new
                                  
