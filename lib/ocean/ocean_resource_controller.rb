a2a6c7abb75bac2929deaa1895dbb1dc8484c413
Peter Bengtson <peter@peterbengtson.com>
2013-09-07 11:55:01 +0200

    Added support for OceanDynamo's exceptions.

--- lib/ocean/ocean_resource_controller.rb
+++ lib/ocean/ocean_resource_controller.rb
@@ -20,15 +20,18 @@ module Ocean
     included do
       if defined? ActiveRecord
         rescue_from ActiveRecord::RecordNotUnique, 
-                    ActiveRecord::StatementInvalid do |x|
+                    ActiveRecord::StatementInvalid,
+                    OceanDynamo::RecordNotSaved do |x|
           render_api_error 422, "Resource not unique"
         end
 
-        rescue_from ActiveRecord::StaleObjectError do |x|
+        rescue_from ActiveRecord::StaleObjectError,
+                    OceanDynamo::RecordInConflict do |x|
           render_api_error 409, "Stale #{x.record.class.name}"
         end
 
-        rescue_from ActiveRecord::RecordInvalid do |x|
+        rescue_from ActiveRecord::RecordInvalid,
+                    OceanDynamo::RecordInvalid do |x|
           render_validation_errors x.record, except: ocean_resource_controller_no_validation_errors_on
         end
       end
